a47afd1cf572b22b72aca47734bd8310169234ad
itochax23 <86204563+itochax23@users.noreply.github.com>
2021-10-17 12:37:36 -0500

    Update MechaCarChallenge.R

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,4 @@
-# MechaCarChallenge.RScript
+# Linear Regression to Predict MPG
 
 # load package dplyr
 library(dplyr)
@@ -11,3 +11,16 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 # summary
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data = MechaCar))
+
+# Create Visualizations for the Trip Analysis
+Suspension <- read.csv(file = 'Suspension_coil.csv', check.names = F, stringsAsFactors = F)
+
+total_summary <- Suspension %>% summarize(Mean = mean(PSI), 
+                                           Median = median(PSI),
+                                           Variance = var(PSI),
+                                           Std_Dev = sd(PSI))
+
+lot_summary <- Suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), 
+                                                                        Median = median(PSI),
+                                                                        Variance = var(PSI),
+                                                                        Std_Dev = sd(PSI))
