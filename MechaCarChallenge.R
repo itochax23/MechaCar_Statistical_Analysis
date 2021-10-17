cc6f7803f00fde6267678adb6b55e50877e1fa9d
itochax23 <86204563+itochax23@users.noreply.github.com>
2021-10-17 11:49:43 -0500

    Create MechaCarChallenge.R

---
+++ MechaCarChallenge.R
@@ -0,0 +1,13 @@
+# MechaCarChallenge.RScript
+
+# load package dplyr
+library(dplyr)
+
+# read file
+MechaCar <- read.csv(file = 'MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
+
+# linear regression
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar)
+
+# summary
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data = MechaCar))
